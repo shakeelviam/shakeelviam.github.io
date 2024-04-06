0afa09457476b531a14a35ec55b2220eb0ac7610
shakeelviam <shakeelviam@users.noreply.github.com>
2024-04-06 07:54:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,8 +35,9 @@ function printTicket() {
     let content = document.getElementById('ticket').innerHTML;
     let printWindow = window.open('', '_blank', 'height=600,width=800');
     printWindow.document.write('<html><head><title>Print</title>');
-    printWindow.document.write('<link rel="stylesheet" href="style.css" type="text/css">');
-    printWindow.document.write('</head><body >');
+    printWindow.document.write('<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">');
+    printWindow.document.write('<link rel="stylesheet" href="print.css" type="text/css" media="print">'); // Include print.css for print-specific styles
+    printWindow.document.write('</head><body class="bg-white text-black">'); // Ensure the background is white and text is black for printing
     printWindow.document.write(content);
     printWindow.document.write('</body></html>');
     printWindow.document.close();
